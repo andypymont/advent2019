554937fd5e4e7a9cddd050dfd6f4bed7b6947bb9
Andy Pymont <andypymont@gmail.com>
2019-12-01 11:53:21 +0000

    Day 1 Part 2

--- 01/code.js
+++ 01/code.js
@@ -2,10 +2,20 @@ function fuel_required(mass) {
   return Math.floor(mass / 3) - 2
 }
 
+function fuel_required_including_fuel_mass(mass) {
+  let fuel = 0
+  while ( mass > 0 ) {
+    mass = fuel_required(mass)
+    fuel += Math.max(mass, 0)
+  }
+  return fuel
+}
+
 const sum = (accumulator, current) => (accumulator + current)
 
-function total_fuel_required(input) {
+function total_fuel_required(input, account_for_fuel) {
+  const f = account_for_fuel ? fuel_required_including_fuel_mass : fuel_required
   return input.split('\n').map(x => parseInt(x))
-                          .map(fuel_required)
+                          .map(f)
                           .reduce(sum)
 }
--- 01/tests.js
+++ 01/tests.js
@@ -26,8 +26,31 @@ QUnit.test('total_fuel_required()', function(assert) {
   assert.equal(total_fuel_required(input), 34241, desc)
 })
 
-QUnit.test('Part 1 Solutuon', function(assert) {
+QUnit.test('Part 1 Solution', function(assert) {
   assert.equal(total_fuel_required(puzzle_input),
                3372463,
                'total_fuel_required(puzzle_input) == 3372463')
+})
+
+QUnit.test('fuel_required_including_fuel_mass()', function(assert) {
+  const cases = [
+    { input: 14, expected: 2 },
+    { input: 1969, expected: 966 },
+    { input: 100756, expected: 50346 }
+  ]
+  cases.forEach(function({ input, expected }) {
+    const desc = [
+      'fuel_required_including_fuel_mass(',
+      input,
+      ') == ',
+      expected
+    ].join('')
+    assert.equal(fuel_required_including_fuel_mass(input), expected, desc)
+  })
+})
+
+QUnit.test('Part 2 Solution', function(assert) {
+  assert.equal(total_fuel_required(puzzle_input, true),
+               5055835,
+               'total_fuel_required(puzzle_input, true) == 5055835')
 })
