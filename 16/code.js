f39f8a2f0aa346a648ccd844633c166c1c1fd1a9
Andy Pymont <andypymont@gmail.com>
2019-12-16 13:47:53 +0000

    Day 16 Part 2

--- 16/code.js
+++ 16/code.js
@@ -1,24 +1,51 @@
-const base_pattern = [0, 1, 0, -1]
-function pattern(e) {
-  e += 1 // from base 0 to base 1
-  return Array(base_pattern.length * e).fill()
-                                       .map(function(_, x) {
-                                         return base_pattern[Math.floor(x/e)]
-                                       })
+function phase_digit(current, element, signal) {
+  const pattern = element+1
+  const patternlength = pattern*4
+
+  return Math.abs(signal.map(function(digit, d) {
+    const patternpos = (1 + d) % patternlength
+    const patternpartpos = Math.floor(patternpos/pattern)
+
+    if ( patternpartpos === 1 ) {
+      return digit
+    } else if ( patternpartpos === 3 ) {
+      return -digit
+    } else {
+      return 0
+    }
+  }).reduce((a, b) => a + b)) % 10
 }
 
 function phase(signal) {
-  return signal.map(function(_, e) {
-    const p = pattern(e)
-    return Math.abs(signal.map((dig, d) => dig * p[(d+1) % p.length])
-                          .reduce((a, b) => a + b)) % 10
-  })
+  return signal.map(phase_digit)
 }
 
-function ftf(input, phases) {
+function phase_shortcut(signal) {
+  const output = signal.slice(0)
+  let sum = 0
+
+  for ( let x = (output.length -1); x >= (output.length/2); x-- ) {
+    sum += signal[x]
+    output[x] = sum % 10
+  }
+
+  return output
+}
+
+function fft(input, phases) {
   let signal = input.split('').map(x => parseInt(x))
   for ( let p = 0; p < phases; p++ ) {
     signal = phase(signal)
   }
   return signal.slice(0, 8).join('')
+}
+
+function fft_real(input) {
+  const offset = parseInt(input.slice(0, 7))
+  const initial = input.split('').map(x => parseInt(x))
+  let signal = Array(10000).fill().flatMap(() => initial)
+  for ( let p = 0; p < 100; p++ ) {
+    signal = phase_shortcut(signal)
+  }
+  return signal.slice(offset, offset+8).join('')
 }
--- 16/tests.js
+++ 16/tests.js
@@ -1,15 +1,54 @@
-QUnit.test('pattern(element)', function(assert) {
+QUnit.test('phase_digit(_, digit, signal)', function(assert) {
   const cases = [
-    [0, [0, 1, 0, -1]],
-    [1, [0, 0, 1, 1, 0, 0, -1, -1]],
-    [2, [0, 0, 0, 1, 1, 1, 0, 0, 0, -1, -1, -1]],
-    [3, [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, -1, -1, -1, -1]],
+    [
+      0,
+      [1, 2, 3, 4, 5, 6, 7, 8],
+      4,
+    ],
+    [
+      1,
+      [1, 2, 3, 4, 5, 6, 7, 8],
+      8,
+    ],
+    [
+      2,
+      [1, 2, 3, 4, 5, 6, 7, 8],
+      2,
+    ],
+    [
+      2,
+      [4, 8, 2, 2, 6, 1, 5, 8],
+      0,
+    ],
+    [
+      4,
+      [4, 8, 2, 2, 6, 1, 5, 8],
+      0,
+    ],
+    [
+      5,
+      [4, 8, 2, 2, 6, 1, 5, 8],
+      4,
+    ],
+    [
+      6,
+      [4, 8, 2, 2, 6, 1, 5, 8],
+      3,
+    ]
   ]
-  cases.forEach(function([element, expected]) {
-    assert.deepEqual(
-      pattern(element),
+  cases.forEach(function([digit, signal, expected]) {
+    const desc = [
+      'phase_digit(_, ',
+      digit,
+      ', [',
+      signal.join(', '),
+      ']) === ',
+      expected
+    ].join('')
+    assert.equal(
+      phase_digit('', digit, signal),
       expected,
-      'pattern(element)'
+      desc
     )
   })
 })
@@ -37,7 +76,30 @@ QUnit.test('phase(sequence)', function(assert) {
   })
 })
 
-QUnit.test('ftf(signal, phases)', function(assert) {
+QUnit.test('phase_shortcut(sequence)', function(assert) {
+  const example = [
+    [1, 2, 3, 4, 5, 6, 7, 8],
+    [1, 2, 3, 4, 6, 1, 5, 8],
+    [1, 2, 3, 4, 0, 4, 3, 8],
+    [1, 2, 3, 4, 5, 5, 1, 8],
+    [1, 2, 3, 4, 9, 4, 9, 8],
+  ]
+  const cases = [
+    [example[0], example[1]],
+    [example[1], example[2]],
+    [example[2], example[3]],
+    [example[3], example[4]],
+  ]
+  cases.forEach(function([input, expected]) {
+    assert.deepEqual(
+      phase_shortcut(input),
+      expected,
+      'phase_shortcut([' + input.join(', ') + '])'
+    )
+  })
+})
+
+QUnit.test('fft(signal, phases)', function(assert) {
   const cases = [
     ['80871224585914546619083218645595', 100, '24176176'],
     ['19617804207202209144916044189917', 100, '73745418'],
@@ -45,9 +107,24 @@ QUnit.test('ftf(signal, phases)', function(assert) {
   ]
   cases.forEach(function([signal, phases, expected]) {
     assert.equal(
-      ftf(signal, phases),
+      fft(signal, phases),
       expected,
-      'ftf("' + signal + '", ' + phases + ') === "' + expected + '"'
+      'fft("' + signal + '", ' + phases + ') === "' + expected + '"'
+    )
+  })
+})
+
+QUnit.test('fft_real(signal, phase)', function(assert) {
+  const cases = [
+    ['03036732577212944063491565474664', '84462026'],
+    // ['02935109699940807407585447034323', '78725270'],
+    // ['03081770884921959731165446850517', '53553731'],
+  ]
+  cases.forEach(function([signal, expected]) {
+    assert.equal(
+      fft_real(signal),
+      expected,
+      'fft_real("' + signal + '") === "' + expected + '"'
     )
   })
 })
@@ -55,8 +132,13 @@ QUnit.test('ftf(signal, phases)', function(assert) {
 QUnit.test('Solutions', async function(assert) {
   const input = await fetch_puzzle_input()
   assert.deepEqual(
-    ftf(input, 100),
+    fft(input, 100),
     '40580215',
     'Part 1: 100 phases on puzzle input produces "40580215"'
+  )
+  assert.deepEqual(
+    fft_real(input),
+    '22621597',
+    'Part 2: real FFT algorithm on puzzle input produces "22621597"'
   )
 })
