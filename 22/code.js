4efa6d3b2f8ef374ee2e40cef459da6bb414c0cb
Andy Pymont <andypymont@gmail.com>
2019-12-22 22:34:04 +0000

    Day 22 Part 2 partial implementation: not all tests pass yet

--- 22/code.js
+++ 22/code.js
@@ -1,34 +1,47 @@
-function stack(deck) {
-  return deck.slice(0).reverse()
-}
-
-function cut(deck, cards) {
-  return deck.slice(cards).concat(deck.slice(0, cards))
-}
-
-function deal(deck, increment) {
-  const cards = deck.length
-  const dealt = new Array(cards)
-
-  deck.forEach(function(card, c) {
-    dealt[(c*increment)%cards] = card
+function track_card(card, decksize, instructions) {
+  let position = card
+  instructions.forEach(function(instruction) {
+    const words = instruction.split(' ')
+    if ( words[0] === 'cut' ) {
+      position = (decksize + position - parseInt(words[1])) % decksize
+    } else if ( words[2] === 'increment' ) {
+      position = (parseInt(words[3]) * position) % decksize
+    } else if ( words[3] === 'stack' ) {
+      position = decksize - position - 1
+    }
   })
-
-  return dealt
+  return position
 }
 
-function shuffle(instructions, decksize) {
-  return instructions.reduce(
-    function(deck, instruction) {
-      const words = instruction.split(' ')
-      if ( words[0] === 'cut' ) {
-        return cut(deck, parseInt(words[1]))
-      } else if ( words[2] === 'increment' ) {
-        return deal(deck, parseInt(words[3]))
-      } else if ( words[3] === 'stack' ) {
-        return stack(deck)
-      }
-    },
-    Array(decksize).fill().map((_, i) => i)
-  )
+function gcd_extended(a, b) {
+  let x = 0, y = 1, u = 1, v = 0
+  while ( a !== 0 ) {
+    let q = Math.floor(b / a)
+    [x, y, u, v] = [u, v, x - u * q, y - v * q]
+    [a, b] = [b % a, a]
+  }
+  return [b, x, y]
+}
+
+function modinv(a, m) {
+  const [g, x] = gcd_extended(a, m)
+  if (g !== 1) {
+    throw('Bad mod inverse')
+  }
+  return (x + m) % m
+}
+
+function card_in_position(position, decksize, instructions) {
+  let card = position
+  instructions.reverse().forEach(function(instruction) {
+    const words = instruction.split(' ')
+    if ( words[0] === 'cut' ) {
+      card = (position + parseInt(words[1])) % decksize
+    } else if ( words[2] === 'increment' ) {
+      card = modinv(position, decksize) / parseInt(words[3])
+    } else if ( words[3] === 'stack' ) {
+      card = decksize - card - 1
+    }
+  })
+  return card
 }
--- 22/tests.js
+++ 22/tests.js
@@ -1,156 +1,154 @@
-QUnit.test('stack(deck)', function(assert) {
-  const cases = [
-    {
-      input: [1, 2, 3],
-      expected: [3, 2, 1],
-    },
-    {
-      input: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-      expected: [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
-    },
-    {
-      input: [4, 5, 1, 2, 3, 0],
-      expected: [0, 3, 2, 1, 5, 4],
-    }
-  ]
-  cases.forEach(function({ input, expected }) {
-    const desc = [
-      'stack([',
-      input.join(', '),
-      ']) === [',
-      expected.join(', '),
-      ']',
-    ].join('')
-    assert.deepEqual(stack(input), expected, desc)
+QUnit.test('deal into new stack', function(assert) {
+  const instructions = ['deal into new stack']
+  const expected = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+  expected.forEach(function(card, pos) {
+    assert.equal(track_card(card, 10, instructions), pos)
+    assert.equal(card_in_position(pos, 10, instructions), card)
   })
 })
 
-QUnit.test('cut(deck, cards)', function(assert) {
-  const cases = [
-    {
-      deck: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-      cards: 3,
-      expected: [3, 4, 5, 6, 7, 8, 9, 0, 1, 2],
-    },
-    {
-      deck: [2, 4, 6, 8],
-      cards: 2,
-      expected: [6, 8, 2, 4],
-    },
-    {
-      deck: [1, 2, 3, 4, 5, 6, 7],
-      cards: 5,
-      expected: [6, 7, 1, 2, 3, 4, 5],
-    },
-    {
-      deck: [4, 2, 8, 6],
-      cards: 2,
-      expected: [8, 6, 4, 2],
-    },
-    {
-      deck: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-      cards: -4,
-      expected: [6, 7, 8, 9, 0, 1, 2, 3, 4, 5],
-    }
-  ]
-  cases.forEach(function({ deck, cards, expected }) {
-    const desc = [
-      'cut([',
-      deck.join(', '),
-      '], ',
-      cards,
-      ') === [',
-      expected.join(', '),
-      ']'
-    ].join('')
-    assert.deepEqual(cut(deck, cards), expected, desc)
+QUnit.test('cut cards', function(assert) {
+  const instructions = ['cut 3']
+  const expected = [3, 4, 5, 6, 7, 8, 9, 0, 1, 2]
+  expected.forEach(function(card, pos) {
+    assert.equal(track_card(card, 10, instructions), pos)
+    assert.equal(card_in_position(pos, 10, instructions), card)
   })
 })
 
-QUnit.test('deal(deck, increment)', function(assert) {
-  const cases = [
-    {
-      deck: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-      increment: 3,
-      expected: [0, 7, 4, 1, 8, 5, 2, 9, 6, 3],
-    },
-    {
-      deck: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-      increment: 7,
-      expected: [0, 3, 6, 9, 2, 5, 8, 1, 4, 7],
-    }
-  ]
-  cases.forEach(function({ deck, increment, expected }) {
-    const desc = [
-      'deal([',
-      deck.join(', '),
-      '], ',
-      increment,
-      ') === [',
-      expected.join(', '),
-      ']'
-    ].join('')
-    assert.deepEqual(deal(deck, increment), expected, desc)
+QUnit.test('deal with increment', function(assert) {
+  const instructions = ['deal with increment 3']
+  const expected = [0, 7, 4, 1, 8, 5, 2, 9, 6, 3]
+  expected.forEach(function(card, pos) {
+    assert.equal(track_card(card, 10, instructions), pos)
+    assert.equal(card_in_position(pos, 10, instructions), card)
   })
 })
 
-QUnit.test('shuffle(instructions, decksize)', function(assert) {
-  const cases = [
-    {
-      instructions: [
-        'deal with increment 7',
-        'deal into new stack',
-        'deal into new stack',
-      ],
-      expected: [0, 3, 6, 9, 2, 5, 8, 1, 4, 7],
-      desc: 'first example from puzzle description',
-    },
-    {
-      instructions: [
-        'cut 6',
-        'deal with increment 7',
-        'deal into new stack',
-      ],
-      expected: [3, 0, 7, 4, 1, 8, 5, 2, 9, 6],
-      desc: 'second example from puzzle description',
-    },
-    {
-      instructions: [
-        'deal with increment 7',
-        'deal with increment 9',
-        'cut -2',
-      ],
-      expected: [6, 3, 0, 7, 4, 1, 8, 5, 2, 9],
-      desc: 'third example from puzzle description',
-    },
-    {
-      instructions: [
-        'deal into new stack',
-        'cut -2',
-        'deal with increment 7',
-        'cut 8',
-        'cut -4',
-        'deal with increment 7',
-        'cut 3',
-        'deal with increment 9',
-        'deal with increment 3',
-        'cut -1',
-      ],
-      expected: [9, 2, 5, 8, 1, 4, 7, 0, 3, 6],
-      desc: 'fourth example from puzzle description',
-    }
+QUnit.test('track_card(card, decksize, instructions)', function(assert) {
+  const examples = [
+    [
+      'deal with increment 7',
+      'deal into new stack',
+      'deal into new stack',
+    ],
+    [
+      'cut 6',
+      'deal with increment 7',
+      'deal into new stack',
+    ],
+    [
+      'deal with increment 7',
+      'deal with increment 9',
+      'cut -2'
+    ],
+    [
+      'deal into new stack',
+      'cut -2',
+      'deal with increment 7',
+      'cut 8',
+      'cut -4',
+      'deal with increment 7',
+      'cut 3',
+      'deal with increment 9',
+      'deal with increment 3',
+      'cut -1',
+    ]
   ]
-  cases.forEach(function({ instructions, expected, desc }) {
-    assert.deepEqual(shuffle(instructions, 10), expected, desc)
+  const cases = [
+    [0, 0, 0],
+    [0, 3, 1],
+    [0, 9, 3],
+    [0, 8, 6],
+    [1, 3, 0],
+    [1, 7, 2],
+    [1, 4, 3],
+    [1, 6, 9],
+    [2, 3, 1],
+    [2, 4, 4],
+    [2, 8, 6],
+    [2, 2, 8],
+    [3, 9, 0],
+    [3, 4, 5],
+    [3, 0, 7],
+    [3, 6, 9],
+  ]
+  cases.forEach(function([ example, card, expected ]) {
+    assert.equal(
+      track_card(card, 10, examples[example]),
+      expected,
+      'example ' + (example + 1) + ', card ' + card + ': ' + expected
+    )
+  })
+})
+
+QUnit.test('card_in_position(card, decksize, instructions)', function(assert) {
+  const examples = [
+    [
+      'deal with increment 7',
+      'deal into new stack',
+      'deal into new stack',
+    ],
+    [
+      'cut 6',
+      'deal with increment 7',
+      'deal into new stack',
+    ],
+    [
+      'deal with increment 7',
+      'deal with increment 9',
+      'cut -2'
+    ],
+    [
+      'deal into new stack',
+      'cut -2',
+      'deal with increment 7',
+      'cut 8',
+      'cut -4',
+      'deal with increment 7',
+      'cut 3',
+      'deal with increment 9',
+      'deal with increment 3',
+      'cut -1',
+    ]
+  ]
+  const cases = [
+    [0, 0, 0],
+    [0, 1, 3],
+    [0, 3, 9],
+    [0, 6, 8],
+    [1, 0, 3],
+    [1, 2, 7],
+    [1, 3, 4],
+    [1, 9, 6],
+    [2, 1, 3],
+    [2, 4, 4],
+    [2, 6, 8],
+    [2, 8, 2],
+    [3, 0, 9],
+    [3, 5, 4],
+    [3, 7, 0],
+    [3, 9, 6],
+  ]
+  cases.forEach(function([ example, position, expected ]) {
+    assert.equal(
+      card_in_position(position, 10, examples[example]),
+      expected,
+      'example ' + (example + 1) + ', position ' + position + ': ' + expected
+    )
   })
 })
 
 QUnit.test('Solutions', async function(assert) {
   const instructions = await fetch_puzzle_input_lines()
-  const shuffled = shuffle(instructions, 10007)
   assert.equal(
-    shuffled.indexOf(2019),
+    track_card(2019, 10007, instructions),
     5472,
     'Part 1: position of card 2019 after one shuffle === 5472',
+  )
+  assert.equal(
+    card_in_position(2020, 119315717514047, instructions),
+    0,
+    'Part 2: card in position 2020 after 1 shuffle === 0'
   )
 })
