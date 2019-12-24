32db02b1f3388c449ff5208e1d4c1f14b710cba9
Andy Pymont <andypymont@gmail.com>
2019-12-24 19:48:56 +0000

    Day 24 Part 2

--- 24/code.js
+++ 24/code.js
@@ -1,32 +1,153 @@
 function read_state(state) {
-  return state.join('').split('').map(char => char === '#' ? 1 : 0)
+  return new Map([
+    [
+      0,
+      parseInt(state.join('')
+                    .split('')
+                    .reverse()
+                    .map(x => x === '#' ? 1 : 0)
+                    .join(''), 2)
+    ]
+  ])
 }
 
-function next_minute(state) {
-  return state.map(function(infested, position, others) {
-    const neighbours = [
-      position < 5 ? 0 : others[position - 5],
-      position % 5 === 0 ? 0 : others[position - 1],
-      position % 5 === 4 ? 0 : others[position + 1],
-      position > 19 ? 0 : others[position + 5]
-    ].reduce((a, b) => a + b)
-    if ( infested && neighbours !== 1 ) {
-      return 0
-    } else if ( !infested && (neighbours === 1 || neighbours === 2) ) {
-      return 1
-    } else {
-      return infested
+function neighbours(levelposition, recursive=false) {
+  const [level, position] = levelposition.split(';').map(x => parseInt(x))
+  const neighbours = new Set()
+
+  function addneighbour(lvl, pos) {
+    if ( recursive || lvl === level ) {
+      neighbours.add(lvl + ';' + pos)
     }
-  })
+  }
+
+  if ( position < 5 ) {
+    addneighbour(level-1, 7)
+  } else if ( position === 17 && recursive ) {
+    addneighbour(level+1, 20)
+    addneighbour(level+1, 21)
+    addneighbour(level+1, 22)
+    addneighbour(level+1, 23)
+    addneighbour(level+1, 24)
+  } else {
+    addneighbour(level, position-5)
+  }
+
+  if ( position % 5 === 4 ) {
+    addneighbour(level-1, 13)
+  } else if ( position === 11 && recursive ) {
+    addneighbour(level+1, 0)
+    addneighbour(level+1, 5)
+    addneighbour(level+1, 10)
+    addneighbour(level+1, 15)
+    addneighbour(level+1, 20)
+  } else {
+    addneighbour(level, position+1)
+  }
+
+  if ( position > 19 ) {
+    addneighbour(level-1, 17)
+  } else if ( position === 7 && recursive ) {
+    addneighbour(level+1, 0)
+    addneighbour(level+1, 1)
+    addneighbour(level+1, 2)
+    addneighbour(level+1, 3)
+    addneighbour(level+1, 4)
+  } else {
+    addneighbour(level, position+5)
+  }
+
+  if ( position % 5 === 0 ) {
+    addneighbour(level-1, 11)
+  } else if ( position === 13 && recursive ) {
+    addneighbour(level+1, 4)
+    addneighbour(level+1, 9)
+    addneighbour(level+1, 14)
+    addneighbour(level+1, 19)
+    addneighbour(level+1, 24)
+  } else {
+    addneighbour(level, position-1)
+  }
+
+  return neighbours
 }
 
-function first_repeat(state) {
+function next_minute(state, recursive=false) {
+  const neighbourcount = new Map()
+
+  Array.from(state.entries()).forEach(function([level, substate]) {
+    const digits = substate.toString(2)
+                           .padStart(25, '0')
+                           .split('')
+                           .reverse()
+    digits.forEach(function(infested, position) {
+      if ( infested === '1' ) {
+        const myneighbours = neighbours(level + ';' + position, recursive)
+        myneighbours.forEach(function(neighbour) {
+          const current = neighbourcount.get(neighbour)||0
+          neighbourcount.set(neighbour, current+1)
+        })
+      }
+    })
+  })
+
+  const unique = (item, ix, arr) => arr.indexOf(item) === ix
+  const levels = Array.from(neighbourcount.keys())
+                      .map(x => parseInt(x.split(';')[0]))
+                      .filter(unique)
+
+  return new Map(
+    Array.from(neighbourcount.keys())
+         .map(x => parseInt(x.split(';')[0]))
+         .filter(unique)
+         .map(function(level) {
+           const substate = state.get(level)||0
+           const digits = substate.toString(2)
+                                  .padStart(25, '0')
+                                  .split('')
+                                  .reverse()
+           return [
+             level,
+             parseInt(
+               digits.map(function(infested, position) {
+                 const levelpos = level + ';' + position
+                 const neighbours = neighbourcount.get(levelpos)||0
+                 if ( infested === '1' && neighbours !== 1 ) {
+                   return '0'
+                 } else if ( infested === '0' &&
+                             (neighbours === 1 || neighbours === 2) ) {
+                   return '1'
+                 } else {
+                   return infested
+                 }
+               }).reverse().join(''),
+               2
+             )
+           ]
+         })
+  )
+}
+
+function after_minutes(state, minutes) {
+  return Array(minutes).fill()
+                       .reduce(function(acc) {
+                         return next_minute(acc, true)
+                       }, state)
+}
+
+function biodiversity(state) {
   const visited = new Set()
-  let joined = state.join('')
-  while ( !visited.has(joined) ) {
-    visited.add(joined)
+  while ( !visited.has(state.get(0)) ) {
+    visited.add(state.get(0))
     state = next_minute(state)
-    joined = state.join('')
   }
-  return state
+  return state.get(0)
+}
+
+function total_bugs(state) {
+  return Array.from(state.entries())
+              .flatMap(([lvl, substate]) => substate.toString(2).split(''))
+              .filter(x => x === '1')
+              .length
+
 }
--- 24/tests.js
+++ 24/tests.js
@@ -8,8 +8,7 @@ QUnit.test('read_state(state)', function(assert) {
         '..#..',
         '#....',
       ],
-      expected: [0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1,
-                 0, 0, 0, 0],
+      expected: new Map([[0, 1205552]]),
     },
     {
       input: [
@@ -19,8 +18,7 @@ QUnit.test('read_state(state)', function(assert) {
         '##.##',
         '.##..',
       ],
-      expected: [1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0,
-                 1, 1, 0, 0],
+      expected: new Map([[0, 7200233]]),
     },
     {
       input: [
@@ -30,8 +28,7 @@ QUnit.test('read_state(state)', function(assert) {
         '...#.',
         '#.###',
       ],
-      expected: [1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1,
-                 0, 1, 1, 1],
+      expected: new Map([[0, 30687775]]),
     },
     {
       input: [
@@ -41,8 +38,7 @@ QUnit.test('read_state(state)', function(assert) {
         '#.##.',
         '.##.#',
       ],
-      expected: [1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0,
-                 1, 1, 0, 1],
+      expected: new Map([[0, 23519713]]),
     },
     {
       input: [
@@ -52,69 +48,156 @@ QUnit.test('read_state(state)', function(assert) {
         '.....',
         '##...',
       ],
-      expected: [1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1,
-                 1, 0, 0, 0],
+      expected: new Map([[0, 3165711]]),
     }
   ]
-  cases.forEach(function({ input, expected }, min) {
+  cases.forEach(function({input, expected}) {
+    assert.deepEqual(read_state(input), expected)
+  })
+})
+
+QUnit.test('neighbours(position)', function(assert) {
+  cases = [
+    {
+      input: '0;0',
+      expected: new Set(['0;1', '0;5']),
+    },
+    {
+      input: '0;6',
+      expected: new Set(['0;1', '0;5', '0;7', '0;11']),
+    },
+    {
+      input: '0;10',
+      expected: new Set(['0;5', '0;11', '0;15']),
+    },
+    {
+      input: '0;19',
+      expected: new Set(['0;14', '0;18', '0;24'])
+    },
+    {
+      input: '0;24',
+      expected: new Set(['0;19', '0;23']),
+    }
+  ]
+  cases.forEach(function({ input, expected }) {
     assert.deepEqual(
-      read_state(input),
+      neighbours(input),
       expected,
-      'example after ' + min + ' minutes'
+      'neighbours("' + input + '")',
+    )
+  })
+})
+
+QUnit.test('neighbours(position, recursive=true)', function(assert) {
+  const cases = [
+    {
+      input: '0;6',
+      expected: new Set(['0;1', '0;5', '0;7', '0;11']),
+    },
+    {
+      input: '0;11',
+      expected: new Set(['0;6', '0;16', '0;10', '1;0', '1;5', '1;10', '1;15',
+                         '1;20'])
+    },
+    {
+      input: '2;14',
+      expected: new Set(['2;9', '2;19', '2;13', '1;13']),
+    },
+    {
+      input: '2;17',
+      expected: new Set(['2;18', '2;22', '2;16', '3;20', '3;21', '3;22', '3;23',
+                         '3;24']),
+    },
+    {
+      input: '6;13',
+      expected: new Set(['6;8', '6;14', '6;18', '7;4', '7;9', '7;14', '7;19',
+                         '7;24'])
+    },
+    {
+      input: '3;7',
+      expected: new Set(['3;2', '3;6', '3;8', '4;0', '4;1', '4;2', '4;3',
+                         '4;4'])
+    }
+  ]
+  cases.forEach(function({ input, expected }) {
+    assert.deepEqual(
+      neighbours(input, true),
+      expected,
+      'neighbours("' + input + '", true)',
     )
   })
 })
 
 QUnit.test('next_minute(state)', function(assert) {
-  const cases = [
-    [
-      [0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0,
-       0],
-      [1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0,
-       0],
-    ],
-    [
-      [1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0,
-       0],
-      [1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1,
-       1],
-    ],
-    [
-      [1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1,
-       1],
-      [1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0,
-       1],
-    ],
-    [
-      [1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0,
-       1],
-      [1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0,
-       0],
-    ]
+  const states = [
+    new Map([[0, 1205552]]),
+    new Map([[0, 7200233]]),
+    new Map([[0, 30687775]]),
+    new Map([[0, 23519713]]),
+    new Map([[0, 3165711]])
   ]
-  cases.forEach(function([before, after], min) {
-    assert.deepEqual(
-      next_minute(before),
-      after,
-      'advance from minute ' + min + ' to minute ' + (min+1)
-    )
-  })
+  assert.deepEqual(next_minute(states[0]), states[1])
+  assert.deepEqual(next_minute(states[1]), states[2])
+  assert.deepEqual(next_minute(states[2]), states[3])
+  assert.deepEqual(next_minute(states[3]), states[4])
 })
 
-QUnit.test('first_repeat(state)', function(assert) {
-  const initial = [0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0,
-                   1, 0, 0, 0, 0]
+QUnit.test('after_minutes(state, mins)', function(assert) {
+  const expected = new Map([
+    [-5, 4538692],
+    [-4, 9175816],
+    [-3, 5308485],
+    [-2, 15483418],
+    [-1, 31720217],
+    [0, 2882],
+    [1, 33407782],
+    [2, 6096231],
+    [3, 17859612],
+    [4, 361774],
+    [5, 501039],
+  ])
   assert.deepEqual(
-    first_repeat(initial),
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+    after_minutes(new Map([[0, 1205552]]), 10),
+    expected
   )
+})
+
+QUnit.test('total_bugs(state)', function(assert) {
+  assert.equal(
+    total_bugs(new Map([
+      [-5, 4538692],
+      [-4, 9175816],
+      [-3, 5308485],
+      [-2, 15483418],
+      [-1, 31720217],
+      [0, 2882],
+      [1, 33407782],
+      [2, 6096231],
+      [3, 17859612],
+      [4, 361774],
+      [5, 501039],
+    ])),
+    99
+  )
+})
+
+QUnit.test('biodiversity(initial_state)', function(assert) {
+  const initial = new Map([
+    [0, 1205552],
+  ])
+  assert.equal(biodiversity(initial, false), 2129920)
 })
 
 QUnit.test('Solutions', async function(assert) {
   const initial = await fetch_puzzle_input_lines().then(read_state)
   assert.equal(
-    parseInt(first_repeat(initial).reverse().join(''), 2),
+    biodiversity(initial),
     1113073,
-    'Part 1: first repeated state has biodiversity of 0'
+    'Part 1: puzzle input has biodiversity of 1113073'
+  )
+  assert.equal(
+    total_bugs(after_minutes(initial, 200)),
+    1928,
+    'Part 2: 1928 total bugs after 200 minutes in recursive space'
   )
 })
