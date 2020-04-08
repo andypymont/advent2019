2dde7b9d589066d73f6ab3355387c05a0d2725af
Andy Pymont <andypymont@gmail.com>
2020-04-08 10:03:42 +0100

    Day 18 Part 1: keyrings now binary #s instead of strings

--- 18/code.js
+++ 18/code.js
@@ -10,7 +10,7 @@ function adjacent(coords, walls) {
 
 function graph_of_map(map) {
   const walls = new Set()
-  const objects = new Map()
+  const keylocations = new Map()
   const contents = new Map()
 
   map.forEach(function(line, y) {
@@ -18,9 +18,15 @@ function graph_of_map(map) {
       const coords = [x, y].join(',')
       if ( char === '#' ) {
         walls.add(coords)
-      } else if ( char !== '.' ) {
-        objects.set(char, coords)
-        contents.set(coords, char)
+      } else if ( char === '@' ) {
+        keylocations.set(0, coords)
+      } else if ( /([A-Z])/.test(char) ) {
+        const val = 1 << (char.charCodeAt(0) - 65)
+        contents.set(coords, [val, 'door'])
+      } else if ( /([a-z])/.test(char) ) {
+        const val = 1 << (char.charCodeAt(0) - 97)
+        contents.set(coords, [val, 'key'])
+        keylocations.set(val, coords)
       }
     })
   })
@@ -35,22 +41,19 @@ function graph_of_map(map) {
   }
 
   const floodfill = []
-  Array.from(objects).filter(function([objectname, pos]) {
-    return (objectname === '@' || objectname === objectname.toLowerCase() )
-  }).forEach(function([start, pos]) {
-    floodfill.push([start, pos, new Set([pos]), '', 0])
+  Array.from(keylocations).forEach(function([start, pos]) {
+    floodfill.push([start, pos, new Set([pos]), 0, 0])
   })
 
   while ( floodfill.length > 0 ) {
     let [start, pos, visited, doors, steps] = floodfill.shift()
-    const objecthere = contents.get(pos)||'@'
-    if ( objecthere !== '@' && objecthere !== start ) {
-      if ( objecthere === objecthere.toLowerCase() ) {
-        record_route(start, objecthere, doors, steps)
-      } else {
-        doors = doors + objecthere
-      }
+    const [objecthere, typehere] = contents.get(pos)||[0, 'start']
+    if ( objecthere > 0 && objecthere !== start && typehere === 'key' ) {
+      record_route(start, objecthere, doors, steps)
+    } else if ( typehere === 'door' ) {
+      doors = doors|objecthere
     }
+
     adjacent(pos, walls).forEach(function(neighbour) {
       if ( !visited.has(neighbour) ) {
         const nowvisited = new Set(visited)
@@ -62,7 +65,7 @@ function graph_of_map(map) {
 
   const graph = {}
   Array.from(shortroutes).forEach(function([route, [doors, dist]]) {
-    const [origin, destination] = route.split('->')
+    const [origin, destination] = route.split('->') //.map(i => parseInt(i))
     if ( !(origin in graph)  ) {
       graph[origin] = {}
     }
@@ -75,15 +78,16 @@ function graph_of_map(map) {
 function accessible_paths(graph, position, keyring) {
   const node = graph[position]||{}
   const accessible = []
-  for ( neighbour in node ) {
-    if ( keyring.indexOf(neighbour) === -1 ) {
+
+  Object.keys(node).map(i => parseInt(i)).forEach(function(neighbour) {
+    if ( (neighbour & keyring) === 0 ) {
       const { doors, dist } = node[neighbour]
-      const needkeys = doors.toLowerCase().split('')
-      if ( needkeys.every(k => (keyring.indexOf(k) !== -1)) ) {
+      if ( (doors & keyring) === doors ) {
         accessible.push([neighbour, dist])
       }
     }
-  }
+  })
+
   return accessible
 }
 
@@ -103,12 +107,12 @@ function route_prioritiser() {
     return heap[top]
   }
   function lesser(i, j) {
-    return heap[i][1] < heap[j][1]
+    return heap[i][2] < heap[j][2]
   }
   function swap(i, j) {
     [heap[i], heap[j]] = [heap[j], heap[i]]
-    indexset(heap[i][0], i)
-    indexset(heap[j][0], j)
+    indexset(heap[i][0], heap[i][1], i)
+    indexset(heap[j][0], heap[j][1], j)
   }
   function siftUp(node) {
     node = node||(size() - 1)
@@ -132,37 +136,37 @@ function route_prioritiser() {
     }
     return node
   }
-  function indexkey(route) {
-    return Array.from(route).sort().join('') + '-' + route.slice(-1)
+  function indexkey(route, pos) {
+    return [route, pos].join(',')
   }
-  function indexhas(route) {
-    return index.has(indexkey(route))
+  function indexhas(route, pos) {
+    return index.has(indexkey(route, pos))
   }
-  function indexget(route) {
-    return index.get(indexkey(route))
+  function indexget(route, pos) {
+    return index.get(indexkey(route, pos))
   }
-  function indexset(route, value) {
-    return index.set(indexkey(route), value)
+  function indexset(route, pos, value) {
+    return index.set(indexkey(route, pos), value)
   }
-  function indexdelete(route, value) {
-    return index.delete(indexkey(route), value)
+  function indexdelete(route, pos, value) {
+    return index.delete(indexkey(route, pos))
   }
-  function update(route, newvalue) {
-    let node = indexget(route)
-    const prevvalue = heap[node][1]
+  function update(route, pos, newvalue) {
+    let node = indexget(route, pos)
+    const prevvalue = heap[node][2]
     if ( newvalue < prevvalue ) {
-      heap[node] = [route, newvalue]
+      heap[node] = [route, pos, newvalue]
       return siftUp(node)
     }
     return node
   }
-  function push([route, dist]) {
-    const value = [route, dist]
-    if ( indexhas(route) ) {
-      indexset(route, update(route, dist))
+  function push(value) {
+    const [route, pos, dist] = value
+    if ( indexhas(route, pos) ) {
+      indexset(route, pos, update(route, pos, dist))
     } else {
       heap.push(value)
-      indexset(route, siftUp())
+      indexset(route, pos, siftUp())
     }
     return size()
   }
@@ -173,7 +177,7 @@ function route_prioritiser() {
       swap(top, bottom)
     }
     heap.pop()
-    indexdelete(popped[0])
+    indexdelete(popped[0], popped[1])
     siftDown()
     return popped
   }
@@ -183,28 +187,21 @@ function route_prioritiser() {
 
 function shortest_path(graph) {
   const consider = route_prioritiser()
-  let keycount = 0
-  for ( key in graph ) {
-    if ( key !== '@') {
-      keycount++
-    }
-  }
-  const solutions = []
+  const allkeys = Object.keys(graph).map(i => parseInt(i)).reduce((a, b) => a|b)
 
-  accessible_paths(graph, '@', '').forEach(function([pos, dist]) {
-    consider.push([pos, dist])
+  accessible_paths(graph, 0, 0).forEach(function([pos, dist]) {
+    consider.push([pos, pos, dist])
   })
 
   while ( consider.size() > 0 ) {
-    const [keyring, dist] = consider.pop()
-    if ( keyring.length === keycount ) {
+    const [keyring, pos, dist] = consider.pop()
+    if ( keyring === allkeys ) {
       return dist
     }
-    const pos = keyring.slice(-1)
     accessible_paths(graph, pos, keyring).forEach(function([npos, xtradist]) {
       const nkeyring = keyring + npos
       const ndist = dist + xtradist
-      consider.push([nkeyring, ndist])
+      consider.push([nkeyring, npos, ndist])
     })
   }
 
